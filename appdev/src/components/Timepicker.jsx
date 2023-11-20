77a5c3a101a3bf2c772934a08ca55bc10b8f8950
Jessrey Vincent Garrido <128167254+jessrey619@users.noreply.github.com>
2023-11-20 16:18:17 +0800

    Optimized some parts of the earlier code

--- appdev/src/components/Timepicker.jsx
+++ appdev/src/components/Timepicker.jsx
@@ -6,26 +6,58 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { TimePicker } from '@mui/x-date-pickers/TimePicker';
 
 export default function TimePickerValue(props) {
-    const [value, setValue] = React.useState(dayjs()); // Initial time set to 15:30
+  const [open, setOpen] = React.useState(false);
+  const [value, setValue] = React.useState(''); // Initial time set to 15:30
 
-    const onChange = (newValue) => {
-        // const formattedTime = newValue.format('HH:mm'); // Format the time as 'HH:mm'
-        // NOTE: WHEN YOU PROGRAM THIS FRFR is that you need to get when retrieveing data: $H, $m cuz that is the Hour and Minutes. Ma string man ug i-format
-        props.parentalCallback(newValue);
-    };
+  const minutesStep = 30;
+  const getOpenDialogAriaText = (value) => {
+    return `Select time, increments of ${minutesStep} minutes`;
+  };
+
+  const handleTimePickerChange = (newValue) => {
+    setValue(newValue);
+  };
+
+  const handleOpenDialog = () => {
+    setOpen(true);
+  };
+
+  const handleCloseDialog = () => {
+    setOpen(false);
+  };
+
+  const handleAccept = (newValue) => {
+    props.parentalCallback(newValue);
+    setOpen(false); // Close the dialog after accepting the choice
+  };
+
+  const renderOption = (option, state) => {
+    // Check if the option's hour is outside the desired range
+    const isSelectable = option.hour() >= 8 && option.hour() <= 17;
 
     return (
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DemoContainer sx={{ width: '100%' }} components={['TimePicker']}>
-                <TimePicker
-                    sx={{ backgroundColor: 'white', fontSize: '10%' }}
-                    value={value}
-                    onChange={(newValue) => {
-                        setValue(newValue);
-                        onChange(newValue);
-                    }}
-                />
-            </DemoContainer>
-        </LocalizationProvider>
+      <li {...state} style={{ display: isSelectable ? 'block' : 'none' }}>
+        {option.format('h:mm A')}
+      </li>
     );
+  };
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <TimePicker
+        value={value}
+        onChange={handleTimePickerChange}
+        getOpenDialogAriaText={getOpenDialogAriaText}
+        openTo="minutes"
+        minutesStep={minutesStep}
+        minTime={dayjs().set('hour', 8).set('minute', 30)} // Set minimum time to 8:30 AM
+        maxTime={dayjs().set('hour', 17).set('minute', 0)} // Set maximum time to 5:00 PM
+        open={open}
+        onOpen={handleOpenDialog}
+        onClose={handleCloseDialog}
+        onAccept={handleAccept} // Handle the accept event
+        renderOption={renderOption} // Customize the rendering of each option
+      />
+    </LocalizationProvider>
+  );
 }
